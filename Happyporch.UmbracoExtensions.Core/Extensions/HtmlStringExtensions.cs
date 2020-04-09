d6b816ae873ec9423a05b3c19e6ac5e0f6066aff
Tom van Enckevort <tom@happyporch.com>
2020-04-09 16:35:41 +0100

    Added RemoveSurroundingTag method.

---
+++ Happyporch.UmbracoExtensions.Core/Extensions/HtmlStringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using Umbraco.Core;
+
+namespace HappyPorch.UmbracoExtensions.Core.Extensions
+{
+    public static class HtmlStringExtensions
+    {
+        /// <summary>
+        /// Removes the first surrounding tag of the HTML string.
+        /// </summary>
+        /// <param name="html">HTML string</param>
+        /// <param name="tag">Name of the tag to remove (e.g. "p")</param>
+        /// <returns></returns>
+        public static IHtmlString RemoveSurroundingTag(this IHtmlString html, string tag)
+        {
+            if (html == null)
+            {
+                return html;
+            }
+
+            return new HtmlString(html.ToString().TrimStart($"<{tag}>").TrimEnd($"</{tag}>"));
+        }
+    }
+}
