06cd869b88c947517cd90fb6cbd8535117da3055
Filipe Sousa <filipe@endzone.io>
2020-04-22 15:51:23 +0100

    Methods that render elements now keep page.Model in ViewData dictionary.

--- Happyporch.UmbracoExtensions.Core/Extensions/PartialViewExtensions.cs
+++ Happyporch.UmbracoExtensions.Core/Extensions/PartialViewExtensions.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web.Mvc;
@@ -15,35 +12,37 @@ namespace HappyPorch.UmbracoExtensions.Core.Extensions
         /// <summary>
         /// Renders a partial view from a IPublishedElement by convention.
         /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="page"></param>
         /// <param name="element"></param>
         /// <param name="folder"></param>
-        public static void RenderElement<T>(this UmbracoViewPage<T> page, IPublishedElement element, string folder = null)
+        /// <param name="viewData"></param>
+        public static void RenderElement<T>(this UmbracoViewPage<T> page, IPublishedElement element, string folder = null, ViewDataDictionary viewData = null)
         {
             if (element != null)
             {
                 var folderString = folder == null ? "" : folder + "/";
                 var partialView = $"{folderString}{element.ContentType.Alias}";
-                page.Html.RenderPartial(partialView, element);
+                page.Html.RenderPartial(partialView, element, viewData);
             }
         }
 
         /// <summary>
         /// Renders partial views from a set of IPublishedElement(s) by convention.
         /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="page"></param>
-        /// <param name="element"></param>
+        /// <param name="elements"></param>
         /// <param name="folder"></param>
         public static void RenderElements<T>(this UmbracoViewPage<T> page, IEnumerable<IPublishedElement> elements, string folder = null)
         {
-            if (elements != null && elements.Any())
+            if (elements == null || !elements.Any())
             {
-                var folderString = folder == null ? "" : folder + "/";
-                foreach (var element in elements)
-                {
-                    var partialView = $"{folderString}{element.ContentType.Alias}";
-                    page.Html.RenderPartial(partialView, element, new System.Web.Mvc.ViewDataDictionary { { "Elements", elements } });
-                }
+                return;
+            }
+            foreach (var element in elements)
+            {
+                page.RenderElement(element, folder, new System.Web.Mvc.ViewDataDictionary { { "Elements", elements }, { "ViewModel", page.Model } });
             }
         }
     }
