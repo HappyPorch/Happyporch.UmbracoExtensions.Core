d146b57608f68bb77df50b2a33cdfbfb65165b51
Tom van Enckevort <tom@happyporch.com>
2020-07-07 10:39:57 +0100

    Removed generics again.

--- Happyporch.UmbracoExtensions.Core/Extensions/ViewDataDictionaryExtensions.cs
+++ Happyporch.UmbracoExtensions.Core/Extensions/ViewDataDictionaryExtensions.cs
@@ -10,16 +10,6 @@ namespace HappyPorch.UmbracoExtensions.Core.Extensions
         /// </summary>
         /// <param name="viewData"></param>
         /// <returns></returns>
-        public static System.Web.Mvc.ViewDataDictionary Clone<T>(this System.Web.Mvc.ViewDataDictionary<T> viewData)
-        {
-            return Clone(viewData);
-        }
-
-        /// <summary>
-        /// Clones the existing ViewDataDictionary into a new object.
-        /// </summary>
-        /// <param name="viewData"></param>
-        /// <returns></returns>
         public static System.Web.Mvc.ViewDataDictionary Clone(this System.Web.Mvc.ViewDataDictionary viewData)
         {
             if (viewData == null)
@@ -28,20 +18,6 @@ namespace HappyPorch.UmbracoExtensions.Core.Extensions
             }
 
             return new System.Web.Mvc.ViewDataDictionary(viewData);
-        }
-
-        /// <summary>
-        /// Extends the existing ViewDataDictionary with the specified items.
-        /// Example:
-        /// Pass a new ViewData object to a partial view and extend it with a custom value:
-        /// @Html.Partial("MyPartial", Model, ViewData.Clone().Extend(new KeyValuePair&lt;string, object&gt;("IsMobileNav", true))
-        /// </summary>
-        /// <param name="viewData"></param>
-        /// <param name="items"></param>
-        /// <returns></returns>
-        public static System.Web.Mvc.ViewDataDictionary Extend<T>(this System.Web.Mvc.ViewDataDictionary<T> viewData, params KeyValuePair<string, object>[] items)
-        {
-            return Extend(viewData, items);
         }
 
         /// <summary>
