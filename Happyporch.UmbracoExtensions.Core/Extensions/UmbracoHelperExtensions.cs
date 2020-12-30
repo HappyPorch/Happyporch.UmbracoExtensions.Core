9919c68b109eccde684159556d12cd2d9e9ed578
Tom van Enckevort <tom@happyporch.com>
2020-12-30 17:03:53 +0000

    Added file hash helper method.

--- Happyporch.UmbracoExtensions.Core/Extensions/UmbracoHelperExtensions.cs
+++ Happyporch.UmbracoExtensions.Core/Extensions/UmbracoHelperExtensions.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web.Hosting;
+using Umbraco.Core.Cache;
 using Umbraco.Web;
+using Umbraco.Web.Composing;
 
 namespace HappyPorch.UmbracoExtensions.Core.Extensions
 {
@@ -16,6 +17,49 @@ namespace HappyPorch.UmbracoExtensions.Core.Extensions
                 return string1;
             }
             return string2;
+        }
+
+        /// <summary>
+        /// Gets SHA256 file hash for the specified file. Used to add a hash to the static assets URLs to clear the browser cache:
+        /// /assets/css/main.css?hash=@Umbraco.GetFileHash("/assets/css/main.css") =>
+        /// /assets/css/main.css?hash=DA1CC0B5FB4E777FE556FB8D61168C
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string GetFileHash(this UmbracoHelper helper, string filePath)
+        {
+            return Current.AppCaches.RuntimeCache.GetCacheItem($"FileHash-{filePath}", () =>
+            {
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    return null;
+                }
+
+                var serverFilePath = HostingEnvironment.MapPath(filePath);
+
+                if (!File.Exists(serverFilePath))
+                {
+                    return null;
+                }
+
+                byte[] hash;
+
+                using (var sha = SHA256.Create())
+                using (var stream = File.OpenRead(serverFilePath))
+                {
+                    hash = sha.ComputeHash(stream);
+                }
+
+                var hashOutput = new StringBuilder(hash.Length);
+
+                for (var i = 0; i < hash.Length - 1; i++)
+                {
+                    hashOutput.Append(hash[i].ToString("X2"));
+                }
+
+                return hashOutput.ToString();
+            });
         }
     }
 }
