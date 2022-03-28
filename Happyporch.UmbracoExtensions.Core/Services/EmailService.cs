21b8d18fbf73e64feb52ffb0e8b0d5358deccd5b
Tom van Enckevort <tom@happyporch.com>
2022-03-28 13:41:46 +0100

    Added extra overload for sending emails with files

--- Happyporch.UmbracoExtensions.Core/Services/EmailService.cs
+++ Happyporch.UmbracoExtensions.Core/Services/EmailService.cs
@@ -24,6 +24,27 @@ namespace HappyPorch.UmbracoExtensions.Core.Services
         /// <param name="replyToName">Display name</param>
         public static void SendEmail(string fromEmailAddress, IEnumerable<string> toEmailAddresses, string subject, string body, IEnumerable<string> ccEmailAddresses = null, IEnumerable<string> bccEmailAddresses = null, bool isBodyHtml = false, HttpFileCollection files = null, string fromName = "", string replyTo = "", string replyToName = "")
         {
+            var filesWrapper = new HttpFileCollectionWrapper(files);
+
+            SendEmail(fromEmailAddress, toEmailAddresses, subject, body, ccEmailAddresses, bccEmailAddresses, isBodyHtml, filesWrapper, fromName, replyTo, replyToName);
+        }
+
+        /// <summary>
+        /// Send an email using default SMTP settings
+        /// </summary>
+        /// <param name="fromEmailAddress"></param>
+        /// <param name="toEmailAddresses"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="ccEmailAddresses"></param>
+        /// <param name="bccEmailAddresses"></param>
+        /// <param name="isBodyHtml"></param>
+        /// <param name="files"> Use Request.Files["filename"] to get file. Form needs to have enctype="multipart/form-data".</param>
+        /// <param name="fromName">Display name</param>
+        /// <param name="replyTo"></param>
+        /// <param name="replyToName">Display name</param>
+        public static void SendEmail(string fromEmailAddress, IEnumerable<string> toEmailAddresses, string subject, string body, IEnumerable<string> ccEmailAddresses = null, IEnumerable<string> bccEmailAddresses = null, bool isBodyHtml = false, HttpFileCollectionBase files = null, string fromName = "", string replyTo = "", string replyToName = "")
+        {
             // Skips sending email. For testing on ghost inspector. 
             if (body != null && body.Contains("[skip email]")) { return; }
             using (var smtpClient = new SmtpClient())
@@ -59,7 +80,7 @@ namespace HappyPorch.UmbracoExtensions.Core.Services
                 {
                     foreach (string key in files)
                     {
-                        HttpPostedFile file = files[key];
+                        HttpPostedFileBase file = files[key];
                         if (file == null || file.ContentLength == 0) continue;
                         string fileName = Path.GetFileName(file.FileName);
                         var attachment = new Attachment(file.InputStream, fileName);
