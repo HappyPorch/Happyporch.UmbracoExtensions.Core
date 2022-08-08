ddcd294aae2d476ee9705ecc7f5a997d215c83cc
Dejan Mihajlovikj <dejan@happyporch.com>
2022-08-08 16:47:33 +0200

    Clear the cached image quality setting

--- Happyporch.UmbracoExtensions.Core/Components/ImageProcessorComponent.cs
+++ Happyporch.UmbracoExtensions.Core/Components/ImageProcessorComponent.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using Umbraco.Core;
 using Umbraco.Core.Composing;
+using Umbraco.Core.Events;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+
 
 namespace HappyPorch.UmbracoExtensions.Core.Components
 {
@@ -22,6 +26,8 @@ namespace HappyPorch.UmbracoExtensions.Core.Components
         {
             // Validate incoming image processor requests
             ImageProcessingModule.ValidatingRequest += ImageProcessingModule_ValidatingRequest;
+            // Clear the cached image quality whenever website settings is published
+            ContentService.Published += ClearImageCompression;
         }
 
         /// <summary>
@@ -39,6 +45,19 @@ namespace HappyPorch.UmbracoExtensions.Core.Components
                 queryCollection.Remove(null);
 
                 e.QueryString = queryCollection.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Clear the cached image quality whenever website settings is published
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearImageCompression(IContentService sender, ContentPublishedEventArgs e)
+        {
+            if (e.PublishedEntities.Any(x => x.HasProperty("imageCompression")))
+            {
+                Current.AppCaches.RuntimeCache.Clear(ApplicationConstants.ImageQuality);
             }
         }
 
